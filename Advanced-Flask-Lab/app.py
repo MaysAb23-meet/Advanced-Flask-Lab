4ee99eaf3eeed51a4eefb08e8291258de860b58f
MaysAb23-meet <109899447+MaysAb23-meet@users.noreply.github.com>
2022-07-27 10:01:56 +0300

    asd

--- Advanced-Flask-Lab/app.py
+++ Advanced-Flask-Lab/app.py
@@ -25,7 +25,7 @@ posts = {
 
 @app.route('/')  # '/' for the default page
 def home():
-    return render_template('index.html')
+    return render_template('index.html' , imgValue="image_link")
 
 
 @app.route('/about')  # '/' for the default page

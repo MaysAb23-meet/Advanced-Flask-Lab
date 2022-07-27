e7d2de2a0ac94a292ecd24f78d739c1c14d3d2d8
MaysAb23-meet <109899447+MaysAb23-meet@users.noreply.github.com>
2022-07-27 11:58:30 +0300

    flask

--- Advanced-Flask-Lab/app.py
+++ Advanced-Flask-Lab/app.py
@@ -25,7 +25,7 @@ posts = {
 
 @app.route('/')  # '/' for the default page
 def home():
-    return render_template('index.html' , imgValue="image_link")
+    return render_template('index.html' , image_link=image_link)
 
 
 @app.route('/about')  # '/' for the default page
